bfd163f9b0e23d5f27e82184136feb5bf5c10373
Rodrigo <r.nicodella12@gmail.com>
2025-02-28 00:39:39 -0300

    first commit

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,15 @@
+
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography >Fugiat labore sit pariatur ipsum ea ea culpa aliquip dolore elit nulla Lorem ex. Dolore magna ipsum voluptate veniam et commodo ipsum nisi eu occaecat eiusmod. Commodo nulla mollit anim esse qui ut tempor. Dolor duis dolor id velit consequat.</Typography>
+    </JournalLayout>
+  )
+}
+
+
